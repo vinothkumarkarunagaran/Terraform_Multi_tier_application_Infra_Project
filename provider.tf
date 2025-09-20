cdfb04c862a434b49c0189c96778a3733524cb98
vinothkumarkarunagaran <164986303+vinothkumarkarunagaran@users.noreply.github.com>
2025-09-20 21:23:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "vino-s3-bucket-1"
     key            = "multitier/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
